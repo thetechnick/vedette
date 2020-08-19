76aa19f3c91592a9e7f250132a38a2cc78d7c070
Nico Schieder <code@nico-schieder.de>
2020-08-19 14:37:11 +0200

    Generate InterfaceProvisioner type

--- main.go
+++ main.go
@@ -74,6 +74,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "InterfaceInstance")
 		os.Exit(1)
 	}
+	if err = (&controllers.InterfaceProvisionerReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("InterfaceProvisioner"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "InterfaceProvisioner")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
